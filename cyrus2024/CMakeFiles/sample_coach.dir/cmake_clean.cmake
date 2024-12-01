10da8d776864eb7a36bfbe356c2c0340fb3821a8
naderzare <nader.zare88@gmail.com>
2024-12-01 19:34:21 -0400

    add cyrus

---
+++ cyrus2024/CMakeFiles/sample_coach.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_coach.dir/main_coach.cpp.o"
+  "CMakeFiles/sample_coach.dir/sample_coach.cpp.o"
+  "CMakeFiles/sample_coach.dir/sample_freeform_message.cpp.o"
+  "sample_coach"
+  "sample_coach.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_coach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
